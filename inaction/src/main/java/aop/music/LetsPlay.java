a35b74f2d3a54ef3c82bcd403b7114d949037523
y.dubovitsky <y.dubovitsky@gmail.con>
2019-07-26 08:45:52 +0300

    module "in-action" chapter aop
    DONE:
    
    - fixed!
    - https://stackoverflow.com/questions/13461985/classcastexception-proxy-cannot-be-cast-to-using-aop
    - Does your SaleRoom class implement some interface? If yes, then you should use interface and not class in you code:
    
      ISaleRoom saleRoom = (ISaleRoom) context.getBean(saleName);
      Because if your bean implements some interface then Spring by default will create proxy based on this interface.

--- inaction/src/main/java/aop/music/LetsPlay.java
+++ inaction/src/main/java/aop/music/LetsPlay.java
@@ -8,7 +8,7 @@ public class LetsPlay {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("aop.music/config.xml");
 
-        Baritone baritone = (Baritone) context.getBean("baritone");
+        Musician baritone = (Musician) context.getBean("baritone");
         baritone.play();
     }
 }
