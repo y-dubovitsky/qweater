0c726b8d9fdf7d8814e9b4c1c1742197515351cd
y.dubovitsky <y.dubovitsky@gmail.com>
2020-03-03 22:33:40 +0300

    Deleted an unnecessary module and created a new module with a theory.

--- qweater/src/main/resources/sql/time_zone.sql
+++
@@ -1,7 +0,0 @@
--- From mysql workbench run the following sql statements:
-
-SET @@global.time_zone = '+00:00';
-SET @@session.time_zone = '+00:00';
--- with the following sql statements check if the values were set:
-
--- SELECT @@global.time_zone, @@session.time_zone;
--- qweater/src/main/resources/templates/greeting.ftl
+++
@@ -1,5 +0,0 @@
-<#import "parts/common.ftl" as c>
-<@c.page>
-<div>Hello, author</div>
-<a href="/main">Main Page</a>
-</@c.page>
