016ddbe5dff71196c2bb6fc5201ce27b0061b3e4
y.dubovitsky <y.dubovitsky@gmail.con>
2020-07-24 12:48:27 +0300

    Replaced java configuration with annotations.

--- doit/project2/src/main/java/space/dubovitsky/ioc/component/impl/MessageProviderImpl.java
+++ doit/project2/src/main/java/space/dubovitsky/ioc/component/impl/MessageProviderImpl.java
@@ -1,7 +1,9 @@
 package space.dubovitsky.ioc.component.impl;
 
+import org.springframework.stereotype.Component;
 import space.dubovitsky.ioc.component.MessageProvider;
 
+@Component
 public class MessageProviderImpl implements MessageProvider {
 
     public String getMessage() {
--- doit/project2/src/main/java/space/dubovitsky/ioc/component/impl/MessageRenderImpl.java
+++ doit/project2/src/main/java/space/dubovitsky/ioc/component/impl/MessageRenderImpl.java
@@ -1,8 +1,10 @@
 package space.dubovitsky.ioc.component.impl;
 
+import org.springframework.stereotype.Component;
 import space.dubovitsky.ioc.component.MessageProvider;
 import space.dubovitsky.ioc.component.MessageRender;
 
+@Component("messageRender")
 public class MessageRenderImpl implements MessageRender {
 
     private final MessageProvider messageProvider;
